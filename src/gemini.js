da407752caa8ef2d4aebbcefff19fd5039de8355
ujjwal <ujjwaltomar.0808@gmail.com>
2024-12-04 20:56:26 +0530

    add gemini config

---
+++ src/gemini.js
@@ -0,0 +1,34 @@
+let apiKey = "Enter Your Gemini API Key";
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 200,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    return result.response.text();
+  }
+  
+  export default run;
