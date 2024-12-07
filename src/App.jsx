e2fae59d225832f8caf0dbe543bae950c97893aa
ujjwal <ujjwaltomar.0808@gmail.com>
2024-12-07 16:38:57 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext } from 'react';
+import AI from './assets/images/aiRobot.png';
+import AiVoice from './assets/images/ai_voice.gif';
+import { CiMicrophoneOn } from "react-icons/ci";
+import { dataContext } from './context/UserContext';
+import { TbLoader3 } from "react-icons/tb";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let {recognition, speaking, setSpeaking, prompt, setPrompt, response} = useContext(dataContext);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full flex justify-center items-center flex-col">
+      <div className="rounded-full flex justify-center items-center p-[10px] background shadow-xl">
+      <div className="w-[250px] h-[250px] flex justify-center items-center overflow-hidden rounded-full background border-[#fff] border-[5px]">
+        <img className="object-cover w-full rounded-full" src={AI} alt="AI Robot" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <span className=" text-[24px] font-bold text-[#525151] mt-[25px] text-center">I'm AI, Your Advance Virtual Assistant</span>
+      {!speaking ? <button onClick={() => {
+        setPrompt("Listening...")
+        setSpeaking(true);
+        recognition.start();
+      }} className="text-[15px] uppercase font-bold border-[1px] border-[#525151] text-[#525151] rounded-[10px] px-[35px] py-[10px] mt-[25px] flex items-center">
+        Start
+        <CiMicrophoneOn className="text-[20px] font-bold text-" />
+      </button> : <div className="w-full flex flex-col justify-center items-center gap-4 mt-[10px]">
+        {!response ? <TbLoader3 className="text-[54px] animate-spin text-[#525151]" /> : <img src={AiVoice} alt="ai_response" className="w-[150px]" />}
+          <p className="text-[14px] font-bold text-[#525151] text-center">{prompt}</p>
+        </div>}
+      
+    </div>
   )
 }
 
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -74,6 +74,103 @@ function UserContext({children}) {
         return translations[targetLang] || text;
     }
 
+    async function takeCommand(command) {
+        if (command.includes("tumhara naam kya hai") || 
+        command.includes("what is your name") || 
+        command.includes("apka naam kya hai")) {
+        
+        let lang = "en";
+        if (command.includes("kya hai")) lang = "hi";
+        if (command.includes("نام")) lang = "ur";
+
+        let response = await translateText("My name is  AI.", lang);
+        speak(response);
+        setPrompt(response);
+        } else if (command.includes("open") && command.includes("youtube")) {
+            window.open("https://www.youtube.com/", "_blank");
+            speak("Opening YouTube");
+            setResponse(true);
+            setPrompt("Opening YouTube...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("facebook")) {
+            window.open("https://www.facebook.com/", "_blank");
+            speak("Opening Facebook");
+            setResponse(true);
+            setPrompt("Opening Facebook...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("google")) {
+            window.open("https://www.google.com/", "_blank");
+            speak("Opening Google");
+            setResponse(true);
+            setPrompt("Opening Google...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("instagram")) {
+            window.open("https://www.instagram.com/", "_blank");
+            speak("Opening Instagram");
+            setResponse(true);
+            setPrompt("Opening Instagram...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("github")) {
+            window.open("https://github.com/", "_blank");
+            speak("Opening GitHub");
+            setResponse(true);
+            setPrompt("Opening GitHub...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("linkedin")) {
+            window.open("https://www.linkedin.com/", "_blank");
+            speak("Opening LinkedIn");
+            setResponse(true);
+            setPrompt("Opening LinkedIn...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("canva")) {
+            window.open("https://www.canva.com/", "_blank");
+            speak("Opening Canva");
+            setResponse(true);
+            setPrompt("Opening Canva...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("vercel")) {
+            window.open("https://vercel.com/", "_blank");
+            speak("Opening Vercel");
+            setResponse(true);
+            setPrompt("Opening Vercel...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("netlify")) {
+            window.open("https://www.netlify.com/", "_blank");
+            speak("Opening Netlify");
+            setResponse(true);
+            setPrompt("Opening Netlify...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else if (command.includes("open") && command.includes("gmail")) {
+            window.open("https://mail.google.com/", "_blank");
+            speak("Opening Gmail");
+            setResponse(true);
+            setPrompt("Opening Gmail...");
+            setTimeout(() => {
+                setSpeaking(false);
+            }, 5000);
+        } else {
+            aiResponse(command);
+        }
+    }
+
     let value = {
         recognition,
         speaking,
